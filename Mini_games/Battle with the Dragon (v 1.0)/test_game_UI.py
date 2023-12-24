bb693c540a3f407df4f73d6bbd97cb5fe0311d15
Sergey-Misiuk <127656852+Sergey-Misiuk@users.noreply.github.com>
2023-12-24 14:49:43 +0300

    Fix and update game

--- Mini_games/Battle with the Dragon (v 1.0)/test_game_UI.py
+++ Mini_games/Battle with the Dragon (v 1.0)/test_game_UI.py
@@ -6,23 +6,29 @@ def attack_point():
 def healing_point():
     return test_game_logic.healing()
 
-def create_player(name_player):
+def create_player():
+    name_player = input('Введите имя персонажа:\n')
     hero = test_game_logic.Player(name_player)
     return hero
+
+def create_enemy():
+    enemy = test_game_logic.Enemy()
+    return enemy
+
 
 def game(player1,enemy):
     try:
         while enemy.health >= 1 and player1.health >=1:
             print(f'\n{player1.name} {player1.health} ед. здоровья ----------- {enemy.name} {enemy.health} ед. здоровья')
-            player_choice = int(input("""Ваши ход:
-                1. Атакавать (нанести урон на 1-10 ед.)
-                2. Вылечить раны (вы восполните свое здоровье на 1-20 ед., но не больше 100)\n"""))
-            if player_choice == 1:
+            player_choice = input("""Ваши ход:
+                1. Атаковать (нанести урон на 1-10 ед.)
+                2. Вылечить раны (вы восполните свое здоровье на 1-20 ед., но не больше 100)\n""")
+            if player_choice == '1':
                 attack_player = attack_point()
                 print(f'\nВы нанесли - {attack_player} ед. урона')
                 enemy.health -= attack_player
                 print(f'У {enemy.name} - {enemy.health} ед. здоровья')
-            elif player_choice == 2:
+            elif player_choice == '2':
                 player_healing = healing_point()
                 print(f"\nВы выпили зелье. Ваше здоровье востановленно на {player_healing} ед.")
                 player1.health += player_healing
@@ -32,7 +38,7 @@ def game(player1,enemy):
                 print(f'У вас {player1.health} ед. здоровья\n')
             else:
                 print('\nВыбирите действие из меню!!!')
-                game(player1,enemy)
+                continue
             
             if enemy.health <= 0:
                 print("Поздравляем!!! Вы победили противника, вы выйграли!\n")
@@ -47,10 +53,8 @@ def game(player1,enemy):
                 break
             
     except:
-        print('\nВыбирите действие из меню!!!')
-        game(player1,enemy)
-
-
+        print("Возникла ошибка. Перезапустите или обратитись в службу поддержки.")        
+        
 
 def main():
     while True:
@@ -60,10 +64,8 @@ def main():
         if choice == '0':
             break
         elif choice == '1':
-            new_player = input('Введите имя персонажа:\n')
-            player1 = create_player(new_player)
-            # player1 = test_game_logic.Player(new_player)
-            enemy = test_game_logic.Enemy('Dragon')
+            player1 = create_player()
+            enemy = create_enemy()
             game(player1,enemy)
         
             
